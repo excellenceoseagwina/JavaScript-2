0487a0568eb9925c3e29d0444846ce887c5d98fe
excellenceoseagwina <oseagwinaexcellence@gmail.com>
2025-08-11 12:23:19 +0100

    commit

---
+++ ch1_alert.js
@@ -0,0 +1,11 @@
+Math.random() // Generates random numbers between 0 - 1
+Math.floor() // Roundup decimal to the nearest interger
+
+
+let num1 = Math.random();
+console.log(num1);
+
+let num2 = Math.floor(50.678);
+console.log(num2);
+
+console.log(Math.floor(Math.random() * 100));
---
+++ introduction.js
@@ -0,0 +1,3 @@
+console.log("Hello World!"); // Display the result
+console.table("Hello World!"); //Display result in tablular format
+console.error("Hello World!"); // Display log  as error format
