88dc843efa51fb51bea7a4beb44ec880f7212cbd
excellenceoseagwina <oseagwinaexcellence@gmail.com>
2025-08-15 20:47:57 +0100

    commit

--- javaScript_essential.js
+++ javaScript_essential.js
@@ -107,3 +107,128 @@ console.log(`${bool} is a type of ${typeof bool}`);
 console.log(sym, "is a type of", typeof sym);
 console.log(`${unassign} is a type of ${typeof unassign}`);
 console.log(`${empty} is a type of ${typeof empty}`);
+
+let nr1 = "false";
+nr1 = Boolean(nr1);
+console.log(nr1);
+
+
+let str_1 = 'Laurence'; 
+let str_2 = "Svekis"; 
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+console.log(str_1, "is a type of", typeof str1);
+console.log(str_2, "is a type of", typeof str_2);
+console.log(val1, "is a type of", typeof val1);
+console.log(val2, "is a type of", typeof val2); 
+console.log(myNum, "is a type of", typeof myNum);
+
+let myName = "Excellence";
+let myAge = 2;
+let canWriteJavascript = true;
+console.log(`Hello, my name is ${myName}, i am ${myAge} years old and i can write Javascript code: ${canWriteJavascript}.`);
+
+//Unary operators: increment and decrement
+let num1 = 1;
+num1++;
+console.log(num1); // ---> 2
+
+let num11 = 2;
+num11--;
+console.log(num11); // ---> 1
+
+let nr_1 = 4;
+let nr2 = 5;
+let nr3 = 2;
+console.log(nr_1++ + ++nr2 * nr3++); // ---> 16;
+
+/* Practice exercise 2.3
+Write some code to calculate the hypotenuse of a triangle using the Pythagorean 
+theorem when given the values of the other two sides. The theorem specifies that the 
+relation between the sides of a right-angled triangle is a2
+ + b2
+ = c2
+*/
+
+let a1 = window.prompt("Value 1?");
+let b1 = window.prompt("Value 2?");
+a1 = Number(a1);
+b1 = Number(b1);
+let hypotenuseVal = ((a1 * a1) + (b1 * b1))**0.5;
+console.log(hypotenuseVal);
+
+let a = 2;
+let b = 4;
+let c = 6;
+a += b; // --> 6
+a /= c; // --> 1
+c %= b; // --> 2
+
+console.log(a);
+console.log(b);
+console.log(c); // -->  2
+
+let x = 5;
+let y = "5";
+console.log(x == y); // --> True
+console.log(x === y); // --> False
+console.log(x != y); // False
+console.log(x !== y); // True
+
+let m = 5;
+let n = 6;
+console.log(n > m); // True
+console.log(m > n); // False
+console.log(m > m); // False
+console.log(n >= n); // True
+
+console.log(n < m); // False
+console.log(m < n); // True
+console.log(n <= n); // True
+
+//Logical operators
+let r = 2;
+let s = 4;
+let t = 6;
+
+//And
+console.log(r < s && s < t); // ---> True 
+console.log(r > s && t < r); // False
+
+//Or
+console.log( r < s || s > t) // --> True 
+console.log(r > s || s > t) // --> False
+
+//Not
+console.log(!(s < t)); // --> False
+console.log(!(s > t)); // True
+
+
+/*Miles-to-kilometers converter
+Create a variable that contains a value in miles, convert it to kilometers, and log the 
+value in kilometers in the following format: 
+The distance of 130 kms is equal to 209.2142 miles*/
+
+let valueInMiles = 100;
+let Converter = valueInMiles * 1.60934;
+console.log(`The distance of ${Converter} is equal to ${valueInMiles}.`);
+
+/*
+BMI calculator
+Set values for height in inches and weight in pounds, then convert the values to 
+centimeters and kilos, outputting the results to the console:
+• 1 inch is equal to 2.54 cm
+• 2.2046 pounds is equal to 1 kilo
+Output the results. Then, calculate and log the BMI: this is equal to weight (in kilos) 
+divided by squared height (in meters). Output the results to the console.*/
+
+let heightInInches = 10;
+let weightInPounds = 60;
+let heightInCM = heightInInches * 2.54;
+let weightInKilo = weightInPounds * 2.2046;
+console.log(`${heightInInches} inches is equal to ${heightInCM} cm`);
+console.log(`${weightInPounds} pounds is equal to ${weightInKilo} kilo`);
+let BMI = weightInKilo/(heightInCM/100*heightInCM/100);
+console.log(BMI);
+
